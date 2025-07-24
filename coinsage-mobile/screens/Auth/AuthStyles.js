75f6d99fd84afd7ac75bc40753dbd2521442c2cb
MrBigroom <blabob9@gmail.com>
2025-07-24 15:30:28 +0800

    Add the template stylesheet for auth screens

---
+++ coinsage-mobile/screens/Auth/AuthStyles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+
+    },
+
+    title: {
+
+    },
+    
+    input: {
+
+    },
+
+    button: {
+
+    },
+
+    buttonText: {
+
+    },
+
+    footerText: {
+
+    },
+
+    link: {
+
+    },
+});
