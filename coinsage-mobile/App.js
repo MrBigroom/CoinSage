7be1400ba658714d368b21d1679a6a04e5cdc43f
MrBigroom <blabob9@gmail.com>
2025-07-25 12:21:09 +0800

    Install formik and yup packages for form validation

--- coinsage-mobile/App.js
+++ coinsage-mobile/App.js
@@ -3,14 +3,13 @@ import { createStackNavigator } from "@react-navigation/native";
 import LoginScreen from "./screens/Auth/LoginScreen";
 import SignupScreen from "./screens/Auth/SignupScreen";
 import Dashboard from "./screens/Dashboard";
-import { HeaderShownContext } from "@react-navigation/elements";
 
 const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login" screenOptions={{ HeaderShown: false }}>
+            <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="Signup" component={SignupScreen} />
                 <Stack.Screen name="Dashboard" component={Dashboard} />
