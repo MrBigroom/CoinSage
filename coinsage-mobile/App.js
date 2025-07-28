8a3c67d8b1e88f7555e3f60e79c3e45d1d729e9a
MrBigroom <blabob9@gmail.com>
2025-07-28 09:32:36 +0800

    Add useSelector to Navigation for protected routes

--- coinsage-mobile/App.js
+++ coinsage-mobile/App.js
@@ -1,5 +1,5 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { NavigationContainer, createStackNavigator } from "@react-navigation/native";
 import LoginScreen from "./screens/Auth/LoginScreen";
 import SignupScreen from "./screens/Auth/SignupScreen";
 import Dashboard from "./screens/Dashboard";
@@ -7,12 +7,22 @@ import Dashboard from "./screens/Dashboard";
 const Stack = createStackNavigator();
 
 export default function App() {
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Signup" component={SignupScreen} />
-                <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                {isAuthenticated ? (
+                    <>
+                        <Stack.Screen name="Dashboard" component={Dashboard} />
+                        <Stack.Screen name="Transactions" component={TransactionsScreen} />
+                    </>
+                ) : (
+                    <>
+                        <Stack.Screen name="Login" component={LoginScreen} />
+                        <Stack.Screen name="Signup" component={SignupScreen} />
+                    </>
+                )}
             </Stack.Navigator>
         </NavigationContainer>
     )
